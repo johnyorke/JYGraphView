3eaa37e74a9261907c59f01badab4bde3956ef4b
John Yorke (MacBook Air) <john.y@somewhat.cc>
2015-03-29 20:54:19 +0100

    Added main app with graph examples

--- JYGraphViewDemoProject/JYGraphViewDemoProject WatchKit Extension/InterfaceController.m
+++ JYGraphViewDemoProject/JYGraphViewDemoProject WatchKit Extension/InterfaceController.m
@@ -7,9 +7,13 @@
 //
 
 #import "InterfaceController.h"
-
+#import "JYGraphView.h"
 
 @interface InterfaceController()
+@property (weak, nonatomic) IBOutlet WKInterfaceGroup *groupOne;
+@property (weak, nonatomic) IBOutlet WKInterfaceGroup *groupTwo;
+@property (weak, nonatomic) IBOutlet WKInterfaceGroup *groupThree;
+@property (weak, nonatomic) IBOutlet WKInterfaceGroup *groupFour;
 
 @end
 
@@ -20,6 +24,45 @@
     [super awakeWithContext:context];
 
     // Configure interface objects here.
+    
+    CGRect rect = CGRectMake(0, 0,self.contentFrame.size.width , 160);
+    NSArray *data = @[@22,@27,@33,@28,@29,@38];
+    
+    JYGraphView *one = [[JYGraphView alloc] initWithFrame:rect];
+    one.graphData = data;
+    one.useCurvedLine = YES;
+    one.backgroundColor = [UIColor whiteColor];
+    one.barColor = [UIColor whiteColor];
+    one.strokeColor = [UIColor blackColor];
+    one.backgroundViewColor = [UIColor whiteColor];
+    one.pointFillColor = [UIColor blackColor];
+    one.labelBackgroundColor = [UIColor clearColor];
+    one.labelFontColor = [UIColor blackColor];
+    one.strokeWidth = 4;
+    [one plotGraphData];
+    [_groupOne setBackgroundImage:[one graphImage]];
+    
+    JYGraphView *two = [[JYGraphView alloc] initWithFrame:rect];
+    two.graphData = data;
+    [two plotGraphData];
+    [_groupTwo setBackgroundImage:[two graphImage]];
+    
+    JYGraphView *three = [[JYGraphView alloc] initWithFrame:rect];
+    three.graphData = data;
+    three.strokeColor = [UIColor orangeColor];
+    three.hidePoints = YES;
+    three.hideLabels = YES;
+    three.barColor = [UIColor clearColor];
+    [three plotGraphData];
+    [_groupThree setBackgroundImage:[three graphImage]];
+    
+    JYGraphView *four = [[JYGraphView alloc] initWithFrame:rect];
+    four.graphData = data;
+    four.hideLines = YES;
+    four.strokeColor = [UIColor purpleColor];
+    four.pointFillColor = [UIColor clearColor];
+    [four plotGraphData];
+    [_groupFour setBackgroundImage:[four graphImage]];
 }
 
 - (void)willActivate {
